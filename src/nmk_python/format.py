b661c1ce95ffd7f9c2f96c80f1860509618b4d69
David Pochet <david.pochet@gmail.com>
2024-10-05 19:02:06 +0200

    Configure tools through pyproject.toml + move to build dependency -- resolves dynod/nmk#180

--- src/nmk_python/format.py
+++ src/nmk_python/format.py
@@ -8,7 +8,7 @@ from nmk.utils import run_with_logs
 class FormatBuilder(NmkTaskBuilder):
     def build(self, src_files: List[str], src_folders: List[str], line_length: int):
         # Delegate to black
-        run_with_logs([sys.executable, "-m", "black", "-l", str(line_length)] + src_files, self.logger)
+        run_with_logs([sys.executable, "-m", "black"] + src_files, self.logger)
 
         # Delegate to isort
         run_with_logs([sys.executable, "-m", "isort"] + src_folders, self.logger)
